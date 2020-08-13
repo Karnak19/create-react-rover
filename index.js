dcf8894dd6996623166feff8c75255e00a467f64
Basile Vernouillet <basile64.v@gmail.com>
2020-08-13 23:42:51 +0200

    possibility to add some deps added !

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 #! /usr/bin/env node
 
+const fs = require("fs");
 const clear = require("clear");
 const figlet = require("figlet");
 const ora = require("ora");
 const chalk = require("chalk");
 const Prompt = require("prompt-checkbox");
+const axios = require("axios");
+
+const packageDeps = require("./availableDeps.json");
 const downloadGist = require("./downloadGist");
 const runCommand = require("./runCommand");
-const axios = require("axios");
 
 const prompt = new Prompt({
   name: "packages",
   message: "Do you want any third-party library ? \n(press space to choose, enter to confirm)\n",
-  choices: ["react-router", "redux", "recoil"],
+  choices: ["react-router", "redux", "recoil", "prop-types"],
 });
 const name = process.argv[2];
 
@@ -50,13 +53,6 @@ const repoURL = "https://github.com/Karnak19/react-caprover-template.git";
 
 let spinner;
 
-const packageDeps = {
-  "react-router": { packages: ["react-router-dom"], id: "e98ca3712bdaa650ce1cf625aa90e495" },
-  redux: { packages: ["redux", "react-redux"], id: "" },
-  recoil: { packages: ["recoil"], id: "" },
-  "prop-types": { packages: ["prop-types"], id: "" },
-};
-
 (async () => {
   const answers = await prompt.run();
   const packages = answers.flatMap((answer) => {
@@ -69,53 +65,36 @@ const packageDeps = {
   await runCommand("npm", ["install"], {
     cwd: process.cwd() + "/" + name,
   });
-  await runCommand("npm", ["install", packages.map((p) => p.pack)], {
+
+  await runCommand("npm", ["install", ...packages.flatMap((p) => p.pack)], {
     cwd: process.cwd() + "/" + name,
   });
 
-  // TODO: Write the files from gist into the project folder
+  const gists = await downloadGist(
+    "https://api.github.com/users/Karnak19/gists",
+    packages.map((p) => p.id)
+  );
+
+  const files = gists.flatMap((gist) => {
+    return Object.values(gist).map((val) => {
+      return {
+        promise: axios.get(val.raw_url),
+      };
+    });
+  });
+
+  const res = await Promise.all(files.map((f) => f.promise));
+
+  res.forEach((f) => {
+    filename = f.request.path.split("/")[5];
+    fs.writeFile(`${process.cwd()}/${name}/src/${filename}`, f.data, (err) => {
+      if (err) throw err;
+    });
+  });
 
   spinner.stop();
   promptEnd();
 })();
-
-// prompt
-//   .run()
-//   .then((answers) => {})
-//   .then(() => {})
-//   .then(() => {})
-//   .then(() => {})
-// .then(() => {
-//   return downloadGist(
-//     "https://api.github.com/users/Karnak19/gists",
-//     packages.map((p) => p.id)
-//   );
-// })
-// .then((gists) => {
-//   // TODO: write to files
-//   const files = gists.map((gist) => {
-//     Object.entries(gist).map(([key, value]) => {
-//       return {
-//         fileName: key,
-//         url: value.raw_url,
-//       };
-//     });
-//   });
-//   console.log(files);
-
-//   return Promise.all(
-//     files.map((file) => {
-//       return axios.get(file.url);
-//     })
-//   );
-// })
-// .then((values) => {
-//   const data = values.map((x) => x.data);
-//   console.log(data);
-// })
-// .then(() => {
-// clear();
-// });
 
 function colorlog(text, color) {
   return console.log(color(text));
